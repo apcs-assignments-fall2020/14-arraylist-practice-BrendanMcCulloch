c17d7d293c62c71df7d4c22218b338e50ecfc1e2
BrendanMcCulloch <22bmcculloch@athenian.org>
2020-11-09 05:11:48 +0000

    made 3 methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,24 +4,47 @@ public class MyMain {
 
     // Counts the number of odd numbers in list
     public static int countOdd(ArrayList<Integer> list) { 
-        // REPLACE WITH YOUR CODE
-        return 0;
+        int c = 0;
+        for (int i = 0; i < list.size(); i++){
+            if (list.get(i) % 2 == 1 ){
+                c +=1;
+            }
+            else if (list.get(i) % 2 == -1){
+                c++;
+            }
+        }
+        return c;
     }
 
     // Returns true if there is an int that appears in both lists
     public static boolean checkDuplicates(ArrayList<Integer> list1, ArrayList<Integer> list2) { 
-        // REPLACE WITH YOUR CODE
+        for (int i = 0; i < list1.size(); i++){
+            int listone = list1.get(i);
+            for (int k = 0; k < list2.size(); i++){
+                int listwo = list2.get(k);
+                if (listone == listwo){
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
     // Takes an int[] as input and returns the equivalent ArrayList<Integer>
     public static ArrayList<Integer> convertToArrayList(int[] arr) { 
-        // REPLACE WITH YOUR CODE
-        return null;
+        ArrayList<Integer> arr_list = new ArrayList<Integer>();
+        for (int i = 0; i < arr.length; i++){
+            arr_list.add(i, arr[i]);
+        }
+        return arr_list;
     }
 
 
     public static void main(String[] args) {
         // Write some code here to test your methods!
+        
+        
+
     }
 }
